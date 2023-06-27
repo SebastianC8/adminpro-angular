90e6a05df8da98ba73d2581dfcc7852e1cf8b198
Sebastián Corrales Sepúlveda <sebasscan@gmail.com>
2023-06-26 21:07:53 -0500

    Primer despliegue

---
+++ src/app/pages/graficauno/graficauno.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graficauno',
+  templateUrl: './graficauno.component.html',
+  styles: [
+  ]
+})
+export class GraficaunoComponent {
+
+}
